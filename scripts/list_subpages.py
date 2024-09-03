1ee9b302305363727d4fe975c47b128b7261cc31
syimyuzya <syimyuzya@gmail.com>
2024-09-04 00:07:07 +0800

    Update sitemap

--- scripts/list_subpages.py
+++ scripts/list_subpages.py
@@ -2,12 +2,14 @@ import requests
 
 username = 'nk2028'
 
-response = requests.get(f'https://api.github.com/users/{username}/repos?type=owner')
+response = requests.get(f'https://api.github.com/users/{username}/repos?type=owner&per_page=100')
 repos = response.json()
 
 excluded_repos = 'hello-world'
 
 for repo in repos:
     if repo['name'] not in excluded_repos and repo['has_pages']:
-        assert repo['homepage'].startswith('https://nk2028.shn.hk/')
+        if not repo['homepage'].startswith('https://nk2028.shn.hk/'):
+            print(f'W: homepage of {repo['name']} is {repo['homepage']}')
+            continue
         print(repo['homepage'])
