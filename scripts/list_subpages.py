15e9ab17ae97c7841c9c53383d7a9ca63a21a377
syimyuzya <syimyuzya@gmail.com>
2024-09-04 11:42:56 +0800

    Update URL for tshet-uinh-tools

--- scripts/list_subpages.py
+++ scripts/list_subpages.py
@@ -1,8 +1,11 @@
 import requests
+import sys
 
 username = 'nk2028'
 
-response = requests.get(f'https://api.github.com/users/{username}/repos?type=owner&per_page=100')
+response = requests.get(
+    f'https://api.github.com/users/{username}/repos?type=owner&per_page=100'
+)
 repos = response.json()
 
 excluded_repos = 'hello-world'
@@ -10,6 +13,8 @@ excluded_repos = 'hello-world'
 for repo in repos:
     if repo['name'] not in excluded_repos and repo['has_pages']:
         if not repo['homepage'].startswith('https://nk2028.shn.hk/'):
-            print(f'W: homepage of {repo['name']} is {repo['homepage']}')
+            print(
+                f'W: homepage of {repo['name']} is {repo['homepage']}', file=sys.stderr
+            )
             continue
         print(repo['homepage'])
